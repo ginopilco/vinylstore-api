bb33f7fed785969ee2428d03580d273acde1283d
Emerson Gino Pilco Parque <gino@MacBook-Air-de-Emerson.local>
2026-04-17 22:56:02 -0500

    api vinylstore

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('products.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}))
+server.use(router)
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`JSON Server corriendo en puerto ${PORT}`)
+})
