2ce58cfd6d9c5c3ed19059f7dbac065eff604448
Emerson Gino Pilco Parque <gino@MacBook-Air-de-Emerson.local>
2026-04-17 23:50:01 -0500

    fix: agregar CORS

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const server = jsonServer.create()
 const router = jsonServer.router('products.json')
 const middlewares = jsonServer.defaults()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+})
+
 server.use(middlewares)
 server.use(router)
 
